dd96ef9e7b624c3128247d4c669bcc15228b1b19
sangam <sangamgrg89@gmail.com>
2023-11-02 21:08:12 +1100

    responsive navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className='text-purple-500'>Hello</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt='Logo image' className='w-1/3' />
+      </div>
+      {/* menu items */}
+
+      <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contacts</li>
+      </ul>
+
+      {/* Hamburger menu */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contacts</li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden'></div>
+    </div>
+  );
+};
+
+export default Navbar;
